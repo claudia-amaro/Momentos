7a1aacf7cbcc1b8c3fb6f78786c0e9aa8364601d
claudia-amaro <claudia.rafaela.amaro@gmail.com>
2017-06-26 21:40:15 +0100

    Registo no Mapa

--- Projeto/public/components/map_register_control.php
+++ Projeto/public/components/map_register_control.php
@@ -3,53 +3,56 @@
 //Ligação à BD
 include_once '../connections/connection.php';
 
-$nome_momento = $_POST['nome_momento'];
-$data_momento = $_POST['data_momento'];
-$historia = $_POST['historia'];
-$latitude = $_POST['latitude'];
-$longitude = $_POST['longitude'];
-
-$id_momento = 34;
-
-
-$query = "UPDATE momentos SET nome_momento=?, data_momento=?, historia=?, latitude=?, longitude=? WHERE id_momento=?";
-
-$stmt = mysqli_prepare($link, $query);
-
-mysqli_stmt_bind_param($stmt, 'ssssss', $nome_momento, $data_momento, $historia, $latitude, $longitude, $id_momento);
-
-if (mysqli_stmt_execute($stmt)) {
-    mysqli_stmt_close($stmt);
-    //	Registo válido.
-
-    header('Location: ../pages/moments.php?');
-} else {
-    mysqli_stmt_close($stmt);
-    // Registo inválido.
-}
-
+//Código correto - Alterar dados
+//
+//$nome_momento = $_POST['nome_momento'];
+//$data_momento = $_POST['data_momento'];
+//$historia = $_POST['historia'];
 //$latitude = $_POST['latitude'];
 //$longitude = $_POST['longitude'];
 //
+//$id_momento = 34;
 //
-//$query = "INSERT INTO momentos (latitude, longitude) VALUES (?,?)";
+//
+//$query = "UPDATE momentos SET nome_momento=?, data_momento=?, historia=?, latitude=?, longitude=? WHERE id_momento=?";
 //
 //$stmt = mysqli_prepare($link, $query);
 //
-//mysqli_stmt_bind_param($stmt, 'ss', $latitude, $longitude);
-//
-//$latitude = $_POST['latitude'];
-//$longitude = $_POST['longitude'];
+//mysqli_stmt_bind_param($stmt, 'ssssss', $nome_momento, $data_momento, $historia, $latitude, $longitude, $id_momento);
 //
 //if (mysqli_stmt_execute($stmt)) {
 //    mysqli_stmt_close($stmt);
 //    //	Registo válido.
 //
-//    header('Location: ../pages/moments.php');
-//}
-//else {
+//    header('Location: ../pages/moments.php?');
+//} else {
 //    mysqli_stmt_close($stmt);
 //    // Registo inválido.
-//
-//    header('Location: ../pages/login_register.php?registo=invalido');
 //}
+
+//Código incorreto - Inserir dados
+
+$latitude = $_POST['latitude'];
+$longitude = $_POST['longitude'];
+
+
+$query = "INSERT INTO momentos (latitude, longitude) VALUES (?,?)";
+
+$stmt = mysqli_prepare($link, $query);
+
+mysqli_stmt_bind_param($stmt, 'ss', $latitude, $longitude);
+
+$latitude = $_POST['latitude'];
+$longitude = $_POST['longitude'];
+
+if (mysqli_stmt_execute($stmt)) {
+    mysqli_stmt_close($stmt);
+    //	Registo válido.
+
+    header('Location: ../pages/moments.php');
+}
+else {
+    mysqli_stmt_close($stmt);
+    // Registo inválido.
+
+}
